5176d901b3449512b2f27cf02fcece65f7fc7914
Chenyse Semper <chenyse.semper@hotmail.com>
2020-11-11 21:10:35 +0000

    Packaged with npm, Added another method for twilio connection

--- reminder/handler.py
+++ reminder/handler.py
@@ -2,7 +2,7 @@ import logging
 import os
 import sys
 import boto3
-from sendsms import connect_to_twilio
+from sendsms import connect_to_twilio, connect_to_twilio_method_2
 from functions import read_csv_file_from_s3, output_member_list, birthday_message
 
 logging.getLogger().setLevel(logging.ERROR)
@@ -29,12 +29,23 @@ def start(event, context):
         logging.error(log_message)
 
     try:
-        #Connect to Twilio & Send SMS
+        #Connect to Twilio with Method 1 & Send SMS
         client = connect_to_twilio()
         publish = client.messages.create(to="+447940025593", 
                        from_="+13158193109", 
                        body= confirmed_message)
-        print (publish)
+        print ("Method 1 Success: {publish}")
+    except Exception as ERROR:
+        log_message = {'SNS Message Failed to Send': {'error': str(ERROR)}}
+        logging.error(log_message)
+    
+      try:
+        #Connect to Twilio with Method 2 & Send SMS
+        client2 = connect_to_twilio_method_2()
+        publish = client2.messages.create(to="+447940025593", 
+                       from_="+13158193109", 
+                       body= confirmed_message)
+        print ("Method 2 Success: {publish}")
     except Exception as ERROR:
         log_message = {'SNS Message Failed to Send': {'error': str(ERROR)}}
         logging.error(log_message)
--- reminder/sendsms.py
+++ reminder/sendsms.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sys
+import twilio
 from twilio.rest import Client
 
 def connect_to_twilio():
@@ -8,4 +9,14 @@ def connect_to_twilio():
     account_sid = "AC1964dad759ab05232bfacc04319b6fc2"
     auth_token = "a9ed21fdb0b610c948f65beb9924b12e"
     client = Client(account_sid, auth_token)
+    return client
+
+def connect_to_twilio_method_2():
+    account_sid = "AC1964dad759ab05232bfacc04319b6fc2"
+    auth_token = "a9ed21fdb0b610c948f65beb9924b12e"
+    
+    try:
+        client = twilio.rest.TwilioRestClient(account_sid, auth_token)
+    except twilio.TwilioRestException as e:
+        print (e)
     return client
